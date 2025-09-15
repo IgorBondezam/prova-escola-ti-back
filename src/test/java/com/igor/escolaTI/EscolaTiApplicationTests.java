f61550008af45fdc7a71045cd2fbdad1e5fc1f13
IgorBondezam <igorbfraca@hotmail.com>
2025-09-15 20:23:36 -0300

    init project

---
+++ src/test/java/com/igor/escolaTI/EscolaTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.escolaTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
